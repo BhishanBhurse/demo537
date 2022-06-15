8a5d90623d6eb3181ef237644ae40d52c6dabe27
bhishan <bhishanbhurse29@gmail.com>
2022-06-15 09:26:17 +0530

    gm

--- caseStatement.sh
+++ caseStatement.sh
@@ -1,11 +1,24 @@
 #! /bin/bash -x
 
-isTrue=true
-case $isTrue in 
-	true)
-		echo "true value"
-	.
-	.
-	.
+read -p "enter the day number: " dayNumber
+
+case $dayNumber in
+	1)
+		echo " monday";;
+	2)
+		echo " tuesday";;
+	3)
+		echo " wednesday";;
+	4)
+                echo " thusday";;
+	5)
+                echo " friday";;
+	6)
+                echo " saturday";;
+	7)
+                echo " sunday";;
 	*)
+		echo "enter correct number";;
+
+esac
 
